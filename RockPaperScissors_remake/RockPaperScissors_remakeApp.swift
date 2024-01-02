9e72b57b051b139870d02674ea7a723e5c5943b7
AT5HK <name@email.com>
2024-01-01 22:52:56 -0800

    Initial Commit

---
+++ RockPaperScissors_remake/RockPaperScissors_remakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissors_remakeApp.swift
+//  RockPaperScissors_remake
+//
+//  Created by AT5HK on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissors_remakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
